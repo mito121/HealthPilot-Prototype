55f62d623cb816f9442c2afe80ffda3bf39f8cc7
Rasmus Andersen <40599932+mito121314@users.noreply.github.com>
2020-01-30 12:06:57 +0100

    Delete protect.php

--- includes/protect.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    session_start();
-    
-    if(!isset($_SESSION['logged']) or empty($_SESSION['logged'])) {
-        $_SESSION = [];
-        header('Location: pages/login.php');
-    }
